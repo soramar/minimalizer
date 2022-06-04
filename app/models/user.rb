b01e242c65ba8873d4449de11f8ef2ab86616512
soramar <fujitanisora0414@i.softbank.jp>
2022-06-04 18:30:48 +0900

    Fix:userバリデーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   validates :email, uniqueness: true, presence: true
   validates :name, presence: true, length: { maximum: 30 }
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/ }, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 end
